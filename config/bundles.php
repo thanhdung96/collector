7edd56f66065fe394bc2fb83e54077bfd0b2e2cb
thanhdung96 <14dh110224@st.huflit.edu.vn>
2024-07-20 15:50:11 +0700

    added swagger and jwt bundles.
    Changes to be committed:
    	modified:   .env
    	modified:   .gitignore
    	new file:   .idea/.gitignore
    	new file:   .idea/collector.iml
    	new file:   .idea/modules.xml
    	new file:   .idea/php.xml
    	new file:   .idea/phpspec.xml
    	new file:   .idea/phpunit.xml
    	new file:   .idea/vcs.xml
    	modified:   composer.json
    	modified:   composer.lock
    	modified:   config/bundles.php
    	new file:   config/packages/lexik_jwt_authentication.yaml
    	new file:   config/packages/security.yaml
    	new file:   config/routes/security.yaml
    	modified:   symfony.lock

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
